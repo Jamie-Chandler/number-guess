aee52c03513e10fc57ad3e1d2b67f5d1e4981248
Jamie <jchandler678@gmail.com>
2020-11-17 12:28:09 +0000

    refactored code to not repeat if statements.

--- script.js
+++ script.js
@@ -22,21 +22,14 @@ document.querySelector(".check").addEventListener("click", function () {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
-    // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too Low! Guess Higher!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You Lose!";
-      document.querySelector(".score").textContent = 0;
-    }
 
-    // When guess is too high
-  } else if (guess > secretNumber) {
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too High! Guess Lower!";
+      document.querySelector(".message").textContent =
+        guess > secretNumber
+          ? "Too High! Guess Lower!"
+          : "Too Low! Guess Higher!";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
